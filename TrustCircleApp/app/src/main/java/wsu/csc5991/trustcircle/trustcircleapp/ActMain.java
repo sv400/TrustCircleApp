91d6b1935a41581d811557e8dce3090795597ce9
Vimal, Sasidharan (DS) <vimal.sasidharan@cdk.com>
2016-07-09 17:23:05 -0400

    Initial add

---
+++ TrustCircleApp/app/src/main/java/wsu/csc5991/trustcircle/trustcircleapp/ActMain.java
@@ -0,0 +1,13 @@
+package wsu.csc5991.trustcircle.trustcircleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.laymain);
+    }
+}
